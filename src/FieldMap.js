d27dcae6cd94f248113138fbc347bec7cd8d1d44
Rohit Singh <rohit.singh1@assurant.com>
2018-02-20 08:42:54 -0500

    use new key

--- src/FieldMap.js
+++ src/FieldMap.js
@@ -4,7 +4,7 @@ import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-map
 
 const MyMapComponent = compose(
   withProps({
-    googleMapURL: 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCugIAkkqEIhrj-wdlgSivOJn2HM6MZCUA&v=3.exp&libraries=geometry,drawing,places',
+    googleMapURL: 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBm7TFrjFh6jFvFu43tcLomNu9CqCMKSOM&v=3.exp&libraries=geometry,drawing,places',
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `400px` }} />,
     mapElement: <div style={{ height: `100%` }} />
